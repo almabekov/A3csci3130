61cdc076970f8f14ce187a23f16f07cfe8604c9c
Andrey Almabekov <almabekov@gmail.com>
2018-06-30 14:52:09 -0300

    now you can edit and delete data

--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -1,6 +1,7 @@
 package com.acme.a3csci3130;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +11,14 @@ public class DetailViewActivity extends Activity {
 
     private EditText nameField, businessNumberField, primaryBusinessField, addressField, provinceField;
     private Button updateButton, deleteButton;
+    private MyApplicationData appState;
     Contact receivedPersonInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
+        appState = ((MyApplicationData) getApplicationContext());
         receivedPersonInfo = (Contact)getIntent().getSerializableExtra("Contact");
 
         updateButton = (Button) findViewById(R.id.updateButton);
@@ -28,16 +31,32 @@ public class DetailViewActivity extends Activity {
 
         if(receivedPersonInfo != null){
             nameField.setText(receivedPersonInfo.name);
+            businessNumberField.setText(receivedPersonInfo.businessNumber);
+            primaryBusinessField.setText(receivedPersonInfo.primaryBusiness);
+            addressField.setText(receivedPersonInfo.address);
+            provinceField.setText(receivedPersonInfo.province);
 
         }
     }
 
     public void updateContact(View v){
         //TODO: Update contact funcionality
+        String name = nameField.getText().toString();
+        String businessNumber = businessNumberField.getText().toString();
+        String primaryBusiness = primaryBusinessField.getText().toString();
+        String address = addressField.getText().toString();
+        String province = provinceField.getText().toString();
+        Contact person = new Contact(receivedPersonInfo.uid, businessNumber, name, primaryBusiness, address, province);
+        appState.firebaseReference.child(receivedPersonInfo.uid).setValue(person);
+        Intent intent=new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     public void eraseContact(View v)
     {
         //TODO: Erase contact functionality
+        appState.firebaseReference.child(receivedPersonInfo.uid).setValue(null);
+        Intent intent=new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
